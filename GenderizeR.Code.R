70c23d73c0726d23c29a65f29d22787a76199759
Bronte <bes7@me.com>
2017-10-31 11:57:03 +1100

    Split name-gender and invert male name-gender probability
    Split names into two data frames, one for male names, and one for
    female ones.
    Inverted probability for male name-gender (line 28).

--- GenderizeR.Code.R
+++ GenderizeR.Code.R
@@ -1,21 +1,32 @@
 setwd("~/Dropbox/Honours/RawData/CSV")
-library("genderizeR", lib.loc="/Library/Frameworks/R.framework/Versions/3.4/Resources/library")
 
-Master <- read.csv("Master.csv") #Imports with 9 NA columns
+if (!require(genderizeR)){
+ install.packages("genderizeR")
+ library(genderizeR)
+}
+
+Master = read.csv("Master.csv") #Imports with 9 NA columns
 Master[,18:27] = NULL #Delete NA columns
 Master$Name = as.character(Master$Name)
 
 
-Name <- c(Master$Name) #Create a new sheet
-Name <- unique(Name) #Get rid of multiples
+Name = c(Master$Name) #Create a new sheet
+Name = unique(Name) #Get rid of multiples
 
-Gender <- c(findGivenNames(Name))
+Gender = c(findGivenNames(Name))
 gdf = data.frame(Name = toupper(Gender$Name), Gender = Gender$Gender, Probability = Gender$Probability) #gdf means gender data frame; toupper changes names to upper case
 gdf$Name = as.character(gdf$Name) #Changes names to characters
 
 #Merge the Master data with the gdf data with a left join to avoid multiples of rows
-NameGender <- merge(x = Master, y = gdf, by = "Name", all.x = TRUE)
+NameGender = merge(x = Master, y = gdf, by = "Name", all.x = TRUE)
 NameGender$Probability = as.numeric(as.character(NameGender$Probability)) #Changes probabilities to numeric
 
 #Visualise the distribution of probabilities of names (where 1 means the function is 100% sure its name-gender estimate is correct, and 0 means it is 0% sure)
 hist(NameGender$Probability)
+
+sdf = split(NameGender, NameGender$Gender) #sdf means split (NameGender) data frame !check if it has been split into two columns!
+sdf$PrMale = 1-sdf$male #should invert the probability of male name-gender; may need brackets
+#Check what the split function named the male column
+#If this doesn't work, save the male-name-probability column as a list, then do 1-x for every element in the list, then merge back into the dataframe
+
+#To run analysis, we need to unsplit the data: combine the male and female probabilities into one column with the corresponding year for each data point
