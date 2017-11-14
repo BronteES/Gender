09e02d9b9c0cb63405a9b50c870067c53b0f584a
Bronte <bes7@me.com>
2017-11-14 13:49:50 +1100

    Tidy and Violin Plot
    Removed redundant code.
    
    Created violin plot (year vs. probability female).
    Created histograms of probability female for each year.

--- GenderizeR.Code.R
+++ GenderizeR.Code.R
@@ -13,25 +13,29 @@ if (!require(purrr)){
  library(purrr)
 }
 
+if (!require(ggplot2)){
+ install.packages("ggplot2")
+ library(ggplot2)
+}
+
 Master = read.csv("Master.csv") #Imports with 9 NA columns
-Master[,18:27] = NULL #Delete NA columns
+Master[,18:27] = NULL #Delete these 9 NA columns
 Master$Name = as.character(Master$Name)
+Master$Year = as.character(Master$Year)
 
 
 Name = c(Master$Name) #Create a new sheet
 Name = unique(Name) #Get rid of multiples
 
-Gender = c(findGivenNames(Name)) #As we have over 1000 names, we have to run this function over two days or pay. Will need to loop for 1-1000, 1001-2000, 2001-3000
-gdf = data.frame(Name = toupper(Gender$name), Gender = Gender$gender, Probability = Gender$probability) #gdf = data.frame(Gender) #gdf means gender data frame; toupper changes names to upper case
-gdf$Name = as.character(gdf$Name) #Change names into characters
+#genderizeR code
+#Gender = c(findGivenNames(Name)) #As we have over 1000 names, we have to run this function over two days or pay. Will need to loop for 1-1000, 1001-2000, 2001-3000
+#gdf = data.frame(Name = toupper(Gender$name), Gender = Gender$gender, Probability = Gender$probability) #gdf = data.frame(Gender) #gdf means gender data frame; toupper changes names to upper case
+#gdf$Name = as.character(gdf$Name) #Change names into characters
 
 #Merge the Master data with the gdf data with a left join to avoid multiples of rows
 NameGender = merge(x = Master, y = gdf, by = "Name", all.x = TRUE, all.y = TRUE)
 NameGender$Probability = as.numeric(as.character(NameGender$Probability)) #Changes probabilities to numeric
-NameGender$Year = as.numeric(as.character((NameGender$Year)))
-
-#Visualise the distribution of probabilities of names (where 1 means the function is 100% sure its name-gender estimate is correct, and 0 means it is 0% sure)
-hist(NameGender$Probability)
+NameGender$Year = as.character((NameGender$Year))
 
 sdf = split(NameGender, NameGender$Gender) #sdf means split (NameGender) data frame !check if it has been split into two columns!
 female = sdf[[1]] #creates a data frame using only the female data
@@ -43,11 +47,6 @@ male.hist = hist(male$prob.female) #histogram of probability male
 female.hist = hist(female$prob.female) #histogram of probability female
 
 PrF = rbind(male, female)
-PrF.hist = hist(PrF$prob.female) #histogram of Probability female
-View(PrF.hist)
-
-scatter.smooth(female$Year, female$Probability) #Scatter plot of female name-gender probability
-scatter.smooth(male$Year, male$invProbability) #Scatter plot of male name-gender probability
 
 Years = split(PrF, PrF$Year)
 y2007 = Years [[1]]
@@ -68,7 +67,34 @@ prop.y2017 = as.data.frame(prop.table(table(y2017$Gender)))
 prop.y2017$Year = c(2017,2017)
 
 prop = rbind(prop.y2007, prop.y2011, prop.y2013, prop.y2015, prop.y2017)
+
 View(prop)
+props = split(prop, prop$Var1)
+fem.prop = props [[1]]
+fem.prop = fem.prop[,-1]
+
+#Smooth graph
+ggplot(data = fem.prop, aes(x = Year, y = Freq, colour = Year)) + 
+ geom_smooth()
+
+#Violin plots for each year
+
+PrF$Year = as.character(PrF$Year)
+ggplot(data = PrF, aes(x = Year, y = prob.female, colour = Year)) + #Setting up the data
+ geom_violin(aes(fill = Year, alpha = 0)) + #Create a violin plot
+ geom_jitter(width = 0.05) + #Show the data points spaced-out
+ scale_x_discrete(breaks = c("2007", "2011", "2013", "2015", "2017"), labels = c("2007", "2011", "2013", "2015", "2017")) + #Year labels
+ xlab("Conference Year") + #x-label
+ ylab("Probability of Female") + #y-label
+ ggtitle("Gender Composition at Evolution Meetings Conferences", subtitle = "Subtitle") + #Titles
+ theme(legend.position = "none") #Remove legend
+#Need to remove the NA violin
+ 
+class(PrF$Year) #character
+class(PrF$prob.female) #numeric
+ 
+
+
 
 PrF.2007.hist = hist(y2007$prob.female)
 PrF.2011.hist = hist(y2011$prob.female)
@@ -76,37 +102,5 @@ PrF.2013.hist = hist(y2013$prob.female)
 PrF.2015.hist = hist(y2015$prob.female)
 PrF.2017.hist = hist(y2017$prob.female)
 
-#Trying out Violin Plots
-#Making a data set with Year and Prob.female
-VYear = PrF$Year
-VProb = as.character(PrF$prob.female)
-V = as.data.frame(VYear, VProb, row.names = NULL, optional = FALSE)
-
-subset = PrF[PrF$Year,PrF$prob.female] #Don't think this is right
-ggplot(PrF$Year, PrF$prob.female)
 
 
-+ geom_line()+geom_violin(aes(group=year),alpha=0.5)
-
-
-
-
-
-
-
-
-
-
-#Not needed. The number of females each year
-femaleyear = split(female, female$Year) #split female data by year
-f2007 = femaleyear[[1]]
-f2011 = femaleyear[[2]]
-f2013 = femaleyear[[3]]
-f2015 = femaleyear[[4]]
-f2017 = femaleyear[[5]]
-count(f2007$Gender) #233
-count(f2011$Gender) #203
-count(f2013$Gender) #332
-count(f2015$Gender) #152
-count(f2017$Gender) #311
-#Doesn't say much as the overall participant numbers fluctuate too.
